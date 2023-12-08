d1f59017f287e58a4d9acd704f49fa2d8f66fddd
Guillermo <nicomano021@gmail.com>
2023-12-08 10:36:54 -0300

    UPDATE

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,20 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) {
         Connection myConn = null;
-        PreparedStatement myStamt = null;
+        Statement myStamt = null;
         ResultSet myRes = null;
 
         try {
             myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "root", "1234");
             System.out.println("Contectado!");
 
-            String sql = ("INSERT INTO employees (id, first_name, last_name) VALUES (?, ?, ?)");
-            myStamt = myConn.prepareStatement(sql);
-            myStamt.setString(1, "2");
-            myStamt.setString(2, "Luis");
-            myStamt.setString(3, "Levratto");
+            myStamt = myConn.createStatement();
 
-            int rowsAffrected = myStamt.executeUpdate();
+            int rowsAffrected = myStamt.executeUpdate("UPDATE employees " + "set first_name = 'Jose Luis'" + "WHERE id = '2'");
+            myRes = myStamt.executeQuery("SELECT * FROM employees order by first_name");
 
-            if (rowsAffrected > 0) {
-                System.out.println("Se ha creado un nuevo empleado");
+            while (myRes.next()) {
+                System.out.println(myRes.getString("first_name")+ " " + myRes.getString("last_name"));
             }
         } catch (Exception e) {
             e.printStackTrace();
